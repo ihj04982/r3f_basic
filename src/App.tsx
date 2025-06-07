4c8a0f99efcfb817d5d8130a8aac91b410868c6e
Heejeong Lim <ihj04982@gmail.com>
2025-06-08 00:08:34 +0900

    Refactor App and ThreeElement components: remove Leva controls, update camera position, and enhance ThreeElement with animated group and celestial bodies.

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,32 @@ import "./App.css";
 import { Canvas } from "@react-three/fiber";
 import { ThreeElement } from "./ThreeElement";
 import { OrbitControls } from "@react-three/drei";
-import { useControls } from "leva";
+// import { useControls } from "leva";
 
 function App() {
-  const color = useControls({
-    value: "green",
-  });
+  // const color = useControls({
+  //   value: "green",
+  // });
 
-  const grid = useControls({
-    segment: { value: 10, min: 2, max: 100, step: 1 },
-  });
+  // const grid = useControls({
+  //   segment: { value: 10, min: 2, max: 100, step: 1 },
+  // });
 
   // 캔버스 안에서는 캔버스 엘리멘트만 사용
   return (
     <>
       <Canvas
         camera={{
-          position: [3, 3, 0],
+          position: [5, 5, 5],
           near: 1,
           far: 100,
           fov: 75,
         }}
       >
-        <color attach="background" args={[color.value]} />
+        <color attach="background" args={["white"]} />
         <OrbitControls />
-        <axesHelper args={[5]} />
-        <gridHelper args={[10, grid.segment]} />
+        <axesHelper args={[6]} />
+        <gridHelper args={[10, 10]} />
         <ThreeElement />
       </Canvas>
       R3F_Basic
--- src/ThreeElement.tsx
+++ src/ThreeElement.tsx
@@ -1,23 +1,60 @@
-import { useControls } from "leva";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+import * as THREE from "three";
+// import { Canvas } from "@react-three/fiber";
 
 export function ThreeElement() {
-  const { color, scale } = useControls({
-    color: "#ffa500",
-    scale: {
-      value: 1,
-      min: 0.1,
-      max: 3,
-      step: 0.1,
-    },
+  // const { scene } = useThree();
+  // const boxRef = useRef<THREE.Mesh>(null);
+  const groupRef = useRef<THREE.Group>(null);
+
+  // const { color, scale } = useControls({
+  //   color: "#ffa500",
+  //   scale: {
+  //     value: 1,
+  //     min: 0.1,
+  //     max: 3,
+  //     step: 0.1,
+  //   },
+  // });
+
+  useFrame((state, delta) => {
+    // if (boxRef.current) {
+    //   boxRef.current.rotation.y += 0.01;
+    // }
+    // scene.rotation.x += 0.01;
+    if (groupRef.current) {
+      groupRef.current.rotation.y += 0.01;
+      // 위치 변경
+      groupRef.current.position.y = Math.sin(state.clock.elapsedTime) * 0.5;
+
+      // 크기 변경
+      groupRef.current.scale.x = 1 + Math.sin(state.clock.elapsedTime) * 0.2;
+
+      // 회전 변경
+      groupRef.current.rotation.y += delta;
+    }
   });
 
   return (
     <>
       <directionalLight position={[10, 10, 0]} />
-      <mesh scale={scale}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color={color} />
-      </mesh>
+      <group ref={groupRef}>
+        {/* 태양 */}
+        <mesh>
+          <sphereGeometry args={[1, 164, 164]} />
+          <meshStandardMaterial color="yellow" />
+        </mesh>
+
+        {/* 지구 궤도 */}
+        <group rotation={[0, Math.PI / 4, 0]}>
+          {/* 지구 */}
+          <mesh position={[3, 0, 0]}>
+            <sphereGeometry args={[0.5, 64, 64]} />
+            <meshStandardMaterial color="blue" />
+          </mesh>
+        </group>
+      </group>
     </>
   );
 }
