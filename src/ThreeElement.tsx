27369be4d6f7b9995be85d7335e9706b8049558c
Heejeong Lim <ihj04982@gmail.com>
2025-06-10 16:27:09 +0900

    remove unused Leva controls, implement mesh geometry sharing within a group, and enhance lighting and material properties for improved visual effects.

--- src/ThreeElement.tsx
+++ src/ThreeElement.tsx
@@ -1,127 +1,77 @@
-import { useControls } from "leva";
+import { useFrame } from "@react-three/fiber";
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
 
 export function ThreeElement() {
-  // const boxRef = useRef<THREE.Mesh>(null);
-  // const boxCopyRef = useRef<THREE.Msesh>(null);
-  // const boxController = useControls({
-  //   width: {
-  //     value: 1,
-  //     min: 0.1,
-  //     max: 10,
-  //     step: 0.1,
-  //   },
-  //   height: {
-  //     value: 1,
-  //     min: 0.1,
-  //     max: 10,
-  //     step: 0.1,
-  //   },
-  //   depth: {
-  //     value: 1,
-  //     min: 0.1,
-  //     max: 10,
-  //     step: 0.1,
-  //   },
-  //   widthSegments: {
-  //     value: 1,
-  //     min: 1,
-  //     max: 10,
-  //     step: 1,
-  //   },
-  //   heightSegments: {
-  //     value: 1,
-  //     min: 1,
-  //     max: 10,
-  //     step: 1,
-  //   },
-  //   depthSegments: {
-  //     value: 1,
-  //     min: 1,
-  //     max: 10,
-  //     step: 1,
-  //   },
-  // });
+  const meshRef = useRef<THREE.Mesh>(null);
 
-  // 두 개의 메시가 동일한 geometry를 공유하도록 설정
-  // useEffect(() => {
-  //   if (boxRef.current && boxCopyRef.current) {
-  //     boxCopyRef.current.geometry = boxRef.current.geometry;
-  //   }
-  // }, [boxController]);
+  const groupRef = useRef<THREE.Group>(null);
 
-  const circleRef = useRef<THREE.Mesh>(null);
-  const circleCopyRef = useRef<THREE.Mesh>(null);
-  const circleController = useControls({
-    radius: {
-      value: 1,
-      min: 0.1,
-      max: 10,
-      step: 0.1,
-    },
-    segments: {
-      value: 32,
-      min: 1,
-      max: 100,
-      step: 1,
-    },
-    thetaStart: {
-      value: 0,
-      min: 0,
-      max: 360,
-      step: 0.01,
-    },
-    thetaLength: {
-      value: 360,
-      min: 0,
-      max: 360,
-      step: 0.01,
-    },
+  useFrame(() => {
+    console.log("frame");
   });
 
   useEffect(() => {
-    if (circleRef.current && circleCopyRef.current) {
-      circleCopyRef.current.geometry = circleRef.current.geometry;
+    for (let i = 0; i < groupRef.current!.children.length; i++) {
+      const childMesh = groupRef.current!.children[i] as THREE.Mesh;
+      childMesh.geometry = meshRef.current!.geometry;
+      childMesh.position.x = i * 2;
     }
-  }, [circleRef, circleCopyRef, circleController]);
+  }, []);
 
   return (
     <>
-      <directionalLight position={[1, 1, 1]} intensity={1} />
-      {/* 커스텀 컴포넌트 */}
-      {/* <mesh ref={boxRef} position={[0, 0, 0]}> */}
-      {/* 인자: 너비, 높이, 깊이, 너비 분할 수, 높이 분할 수, 깊이 분할 수 */}
-      {/* <boxGeometry
-          args={[
-            boxController.width,
-            boxController.height,
-            boxController.depth,
-            boxController.widthSegments,
-            boxController.heightSegments,
-            boxController.depthSegments,
-          ]}
-        /> */}
-      {/* <meshStandardMaterial wireframe /> */}
-      {/* </mesh> */}
-      {/* <mesh ref={boxCopyRef}>
-        <meshStandardMaterial color="red" />
-      </mesh> */}
-      <mesh ref={circleRef} position={[0, 0, 0]}>
-        {/* 인자: 반지름, 분할 수, 시작 각도, 끝 각도 */}
-        <circleGeometry
-          args={[
-            circleController.radius,
-            circleController.segments,
-            THREE.MathUtils.degToRad(circleController.thetaStart),
-            THREE.MathUtils.degToRad(circleController.thetaLength),
-          ]}
-        />
-        <meshStandardMaterial wireframe />
+      <directionalLight position={[5, 5, 5]} intensity={5} />
+
+      <mesh ref={meshRef} position={[0, 0, 0]}>
+        <sphereGeometry args={[1, 10, 10]} />
+        <meshBasicMaterial color="green" visible={false} />
       </mesh>
-      <mesh ref={circleCopyRef}>
-        <meshStandardMaterial color="blue" />
-      </mesh>
+
+      <group ref={groupRef}>
+        <mesh>
+          <meshBasicMaterial color={"green"} wireframe />
+        </mesh>
+        <mesh>
+          {/* 빛에 영향을 받지 않음 */}
+          <meshBasicMaterial
+            color="red"
+            visible // 기본 값 true
+            transparent={true} // 기본 값 false, true시 투명도 조절 가능
+            opacity={1} // 0~1 사이의 값
+            side={THREE.FrontSide} // 기본 값 FrontSide, 면 방향 설정, front, back, double (plane일 때 명확히 보임임)
+            alphaTest={0.5} // opacity의 값과 비교해서 투명도 조절, alphatest보다 작으면 보이지 않음
+            depthTest={true} // 깊이(z buffer) 검사 여부, 기본 값 true, false면 그리기 순서대로 무조건 그림(앞에 그려진 오브젝트를 가릴 수 있음)
+            depthWrite={true} // 깊이(z buffer) 쓰기 여부, 기본 값 true, false면 뒤에 그려지는 오브젝트가 이 오브젝트를 가릴 수 있음
+            fog={true} // 기본값 true, fog의 영향을 받을지 여부
+          />
+        </mesh>
+        {/* fog: color, near, far (거리는 카메라 기준) */}
+        {/* <fog attach="fog" args={["orange", 3, 10]} /> */}
+        <mesh>
+          {/* 빛에 영향을 받음, 나무, 흙, 벽 등 재질 */}
+          <meshLambertMaterial
+            emissive={"red"} // 발광 색상
+            emissiveIntensity={1} // 발광 강도
+            color="blue"
+          />
+        </mesh>
+        {/* ------------------------------------------------------------ */}
+        <mesh>
+          {/* 금속성 재질 */}
+          <meshPhongMaterial
+            color="blue"
+            specular="white" // 반사광
+            shininess={30} // 기본 값 30, 반사광 강도, 높을 수록 sharp 해짐
+            flatShading={false} // true시 면으로 반사 렌더링
+          />
+        </mesh>
+        {/* ------------------------------------------------------------ */}
+        <mesh>
+          {/* Normal 벡터의 xyz 값을 rgb로 표현*/}
+          <meshNormalMaterial />
+        </mesh>
+      </group>
     </>
   );
 }
